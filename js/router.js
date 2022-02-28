3ca1b923949c0355f1d562ce4808dd822d570d45
Glitch (glitch-blank-website) <none>
2022-02-28 23:11:50 +0000

    💆🚐 Checkpoint ./js/router.js:41089821/1409

--- js/router.js
+++ js/router.js
@@ -1,3 +1,45 @@
-class Router(routes) {
-  constructor()
+"use strict";
+
+class Router {
+  constructor(routes) {
+    this.routes = routes;
+    this.rootElem = document.getElementById("app");
+    this.start();
+  }
+  
+  start() {
+    const r = this.routes;
+    (function(scope, r) {
+      window.addEventListener("routechange", () => {
+        scope.hasChanged(scope, r)
+      })
+    });
+    this.hasChanged(this, r);
+  }
+  
+  hasChanged(scope, r) {
+    if(window.location.hash.length > 0) {
+      for(let i = 0; i < r.length; i++) {
+        let route = r[i];
+        if(route.isActiveRoute(window.location.hash.substr(1))) {
+          scope.goToRoute(route.htmlName);
+        }
+      }
+    } else {
+      for(let i = 0; i < r.length; i++) {
+        let route = r[i];
+        if(route.default) {
+          scope.goToRoute(route.htmlName);
+        }
+      }
+    } 
+  }
+  
+  goToRoute(htmlName) {
+    (function(scope) {
+      const url = "views/" + htmlName
+      const xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange
+    }) 
+  }
 }
